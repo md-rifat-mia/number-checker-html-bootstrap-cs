05c13361a8a5ee03a43df3421e5313c7738c642c
MD.RIFAT MIA <mohammadrifatmia16@gmail.com>
2026-07-09 22:31:13 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function checkdata(){
+  let a = document.getElementById("getdata").value;
+if ( a == ''){
+  return alert("fill up blank")
+}
+if ( a == '10'){
+   alert("match")
+}
+else{
+  alert("its wrong")
+}
+
+}
